c8e56ef05b7c5e0dc7cc04372ac9ebe0ba742f06
Ari Moshe <arimoshe@gmail.com>
2022-07-13 10:21:01 -0700

    Create Database

--- aa_questions/import_db.sql
+++ aa_questions/import_db.sql
@@ -1,3 +1,8 @@
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS users;
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE users (
